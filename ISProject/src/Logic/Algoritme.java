37edcc28278a59eaf0d81bedd03defe5e80cc24d
ShimantoRahman <43726554+ShimantoRahman@users.noreply.github.com>
2018-11-23 12:56:55 +0100

    Delete Algoritme.java

--- ISProject/src/Logic/Algoritme.java
+++
@@ -1,7 +0,0 @@
-package Logic;
-
-//interface wordt aangemaakt zodat de code makkelijk kan aangepast worden voor verschillende soorten algoritmes te implementeren
-
-public interface Algoritme {
-    public void startToewijzingsProcedure();
-}
