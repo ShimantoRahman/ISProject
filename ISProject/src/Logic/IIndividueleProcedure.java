5fbde5e41ec46ac951ba7e72fc291932d68c647e
ShimantoRahman <43726554+ShimantoRahman@users.noreply.github.com>
2018-12-08 10:29:14 +0100

    Delete IIndividueleProcedure.java

--- ISProject/src/Logic/IIndividueleProcedure.java
+++
@@ -1,9 +0,0 @@
-package Logic;
-
-import SysteemKlasses.Toewijzingsaanvraag;
-
-import java.util.HashMap;
-
-public interface IIndividueleProcedure {
-    public void startIndividueleProcedure(Toewijzingsaanvraag toewijzingsaanvraag);
-}
